a22f71901780ecd37c4e25c349327c2f76477a07
yoav-wp <nirgaiger@gmail.com>
2015-10-28 15:43:00 +0200

    changed a string from windows

--- Vehicles/Vehicle.m
+++ Vehicles/Vehicle.m
@@ -35,7 +35,7 @@
         degrees = degrees % degreesInACircle;
     }
     
-    return [NSString stringWithFormat:@"Turn %d degrees.", degrees];
+    return [NSString stringWithFormat:@"Turn %d degrees!", degrees];
 }
 
 -(NSString *)changeGears:(NSString *)newGearName
